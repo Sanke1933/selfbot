37fe344aa772bb843cd30226ac27531ad4757266
Sanke1933 <47271449+Sanke1933@users.noreply.github.com>
2020-03-21 22:28:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const client = new Discord.Client();
 client.on('message', message => {
     if (message.content === 'пагнали') {
         console.log('пагнали')
-        setInterval(() => message.channel.send('!work'), 60e4);
+        setInterval(() => message.channel.send('!work'), 61e4);
     }
 
     if (message.content === 'пагнали') {
         console.log('пагнали')
-        setInterval(() => message.channel.send('!crime'), 60e4);
+        setInterval(() => message.channel.send('!crime'), 61e4);
     }
 
     if (message.content === 'пагнали') {
         console.log('пагнали')
-        setInterval(() => message.channel.send('!dep all'), 60e4);
+        setInterval(() => message.channel.send('!dep all'), 61e4);
     }
 });
 
