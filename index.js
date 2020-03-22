ec08c1eb092ac5638d0344c97bf0f40435a9f5eb
Sanke1933 <47271449+Sanke1933@users.noreply.github.com>
2020-03-22 22:44:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const Discord = new require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-    if (message.content === 'пагнали') {
+    if (message.content === 'cука блять нахуй пиздец') {
         console.log('пагнали')
         setInterval(() => message.channel.send('!work'), 61e4);
     }
 
-    if (message.content === 'пагнали') {
+    if (message.content === 'cука блять нахуй пиздец') {
         console.log('пагнали')
         setInterval(() => message.channel.send('!crime'), 61e4);
     }
 
-    if (message.content === 'пагнали') {
+    if (message.content === 'cука блять нахуй пиздец') {
         console.log('пагнали')
         setInterval(() => message.channel.send('!dep all'), 61e4);
     }
